29dd7f925491c46f2ee3a27a5bff4155127524b0
epickard <epickard@gmail.com>
2012-03-29 12:26:36 -0700

    Assignment 4, checkpoint checkin

---
+++ Assignment 4/Hangman.java
@@ -0,0 +1,226 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.awt.*;
+import java.io.*;
+
+public class Hangman extends ConsoleProgram {
+	
+/** number of incorrect guesses in a round */
+private static final int NUM_INCORRECT_GUESSES = 8; //per assignment instructions
+
+/** init added line for line from page 7 of http://see.stanford.edu/materials/icspmcs106a/27-assignment-4-hangman.pdf */
+/** "...Since this is a console program, the console is already installed and will therefore show up in the left column.  
+    When you add the HangmanCanvas it will occupy the second column..." */
+
+//see if the problem is the HangmanCanvas class work is happening before Hangman is getting initialized.
+	public void init() {
+		canvas = new HangmanCanvas();
+		add(canvas);
+	}
+
+    public void run() {
+    	hl = new HangmanLexicon(); //Per instructions
+    	setup();
+    	playGame();
+	}
+    
+    private void setup() {
+    	canvas.reset();
+    	println("Welcome to Hangman!");
+    }
+    
+/*
+ * while player has guesses remaining, run through game's functions. If guesses but no letters remain, player wins. If no guesses remain, 
+ * player loses.
+ */
+    private void playGame() {
+    	wordToGuess = hl.getWord(4);   //secretWord.getWord(rgen.nextInt(0, secretWord.getWordCount()));
+    	numLettersRemaining = wordToGuess.length();
+    	guessesRemaining = NUM_INCORRECT_GUESSES;
+    	playersWord = concatNCopies(wordToGuess.length(), "-"); //method takes whatever string you pass it and concatenates it for n times.
+    	wrongGuessList = "";
+    	
+    	/** while player has guesses, if letters remain, run through game's functions. otherwise guesses remain 
+    	 *  with no letters left, so player wins. */
+    	while (guessesRemaining > 0) {
+    		if (numLettersRemaining > 0){
+    	    	printWord();
+    	    	printGuessesRemaining();
+    	    	acceptGuess();
+    	    	alterWord(wordToGuess, playersWord, "-", letterGuessed);
+    		} else {
+    			printWord();
+    			println("You guessed the word: " + getWordToGuess() + '\n' + "You win.");
+    			break;
+    		}
+    	}
+    	
+    	/** if player has no remaining guesses, (s)he loses. */
+    	if (guessesRemaining == 0) {
+    		println("You're completely hung." + '\n' + "The word was: " + getWordToGuess() + '\n' + "You lose.");
+    	}
+    }
+  
+    
+/*
+ * Prints player's word in its current form, both to the console and the display.
+ */
+    private void printWord() {
+    	println("The word now looks like this: " + playersWord);
+    	canvas.displayWord(playersWord, wrongGuessList);
+    }
+    
+/*
+ * Prints remaining guesses.
+ */
+    private void printGuessesRemaining() {
+    	if (guessesRemaining > 1) {
+    		println("You have " + guessesRemaining + " guesses left.");
+    	} else if (guessesRemaining == 1) {
+    		println("You have only one guess left.");
+    	}
+    }
+    
+    
+/*
+ * Determines validity of player's guess. Guess must be only one letter. No numbers or special characters allowed.   
+ */
+    private void acceptGuess() {
+    	
+    	while (true) {
+    		letterGuessed = readLine("Your guess: ");
+    		
+    		if (!isOneCharGuess(letterGuessed) || !Character.isLetter(guessAsCharacter(letterGuessed))) {
+    			println("Illegal guess. Please try again.");
+    		} else {
+        		letterGuessed = letterGuessed.toUpperCase();
+        		break;
+        	}
+    	} 
+    }
+    
+/*
+ * Checks whether the letter guessed by the player is present in the game's word. If guessed letter is not present, 
+ * returns the player's word unchanged. If it is present, method replaces the dash(es) in the player's word with the letter guessed. 
+ * 
+ * @param ranWord	word that's being guessed.
+ * @param pWord	current state of word player has guessed so far.
+ * @param orig	"dash" in player's word that may be replaced.
+ * @param repl	letter guessed by player.
+ * 
+ * @return	playersWord	current state of player's word after each guessed letter. 
+ */
+    private String alterWord (String ranWord, String pWord, String orig, String repl) {
+    	int index = ranWord.indexOf(repl);
+    	
+    	if (index != -1) {
+    		while (index != -1) {
+    		pWord = pWord.substring(0, index) 
+    			  + repl 
+    			  + pWord.substring(index + orig.length());
+    		index = ranWord.indexOf(repl, index + 1);
+    		numLettersRemaining--;
+    		}
+    		playersWord = pWord;
+    		println("That guess is correct.");
+    		return playersWord;
+    	} else {
+    		println("There are no " + letterGuessed + "'s in the word.");
+    		wrongGuessList += letterGuessed;
+    		canvas.noteIncorrectGuess(guessAsCharacter(letterGuessed));
+    		guessesRemaining--;
+    		return playersWord;
+    	}
+    }
+    
+    
+/*
+ * Concatenates specified number of copies of specified string. Taken directly from Art & Sci of Java, eroberts, page 242.
+ * 
+ * @return result concatenated string of the specified string. Taken directly from Art & Sci of Java, eroberts, page 242.
+ */
+    private String concatNCopies(int n, String str) {
+	    String result = "";
+		    for (int i = 0; i < n; i++) {
+		    result += str;
+    }
+		return result;
+    }
+    
+ /*
+  * Confirms player's guess is no longer than 1 character
+  * 
+  * @param str	guess inputted by the user
+  * 
+  * @return	true if guess contains only one character, false if more than one character OR if no character entered.
+  */
+    private boolean isOneCharGuess(String str) {
+    	int counter = 0;
+    	
+    	for (int i = 0; i < str.length(); i++) {
+    		counter++;
+    	}
+    	
+    	if (counter == 1) return true;
+    	else return false;
+    }
+    
+/*
+ * Converts single-character String to Character
+ * 
+ * @param str single-character String from acceptGuess method.
+ */ 
+    private Character guessAsCharacter(String str) {
+		Character ch = str.charAt(0);
+		return ch;
+    }
+
+    
+/*
+ * Gets the secret word for caller.
+ */
+    public String getWordToGuess() {
+    	return wordToGuess; 
+    }
+    
+/*
+ * Get the number of guesses remaining for caller
+ */
+    public int getGuessesRemaining() {
+    	return guessesRemaining;
+    }
+    
+/*
+ * Get the number of guesses remaining for caller
+ */
+    public int getNumLettersRemaining() {
+    	return numLettersRemaining;
+        }    
+    
+/*
+ * Instance of RandomGenerator to randomly pick a word from HangmanLexicon
+ */
+    private RandomGenerator rgen = new RandomGenerator();
+    
+    
+/** Instance variables for console portion of program */
+    private HangmanLexicon hl;
+    private String wordToGuess;
+    private String letterGuessed;
+    private String playersWord;
+    private String wrongGuessList;
+    private int guessesRemaining;
+    private int numLettersRemaining;
+    
+/** Instance variables for canvas portion of program */
+    private HangmanCanvas canvas;
+}
---
+++ Assignment 4/HangmanCanvas.java
@@ -0,0 +1,230 @@
+/*
+ * File: HangmanCanvas.java
+ * ------------------------
+ * This file keeps track of the Hangman display.
+ */
+
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.util.ErrorException;
+
+public class HangmanCanvas extends GCanvas {
+	
+/* Constants for the simple version of the picture (in pixels) */
+	private static final int SCAFFOLD_HEIGHT = 360;
+	private static final int BEAM_LENGTH = 144;
+	private static final int ROPE_LENGTH = 18;
+	private static final int HEAD_RADIUS = 36;
+	private static final int BODY_LENGTH = 144;
+	private static final int ARM_OFFSET_FROM_HEAD = 28;
+	private static final int UPPER_ARM_LENGTH = 72;
+	private static final int LOWER_ARM_LENGTH = 44;
+	private static final int HIP_WIDTH = 36;
+	private static final int LEG_LENGTH = 108;
+	private static final int FOOT_LENGTH = 28;
+
+/*
+ * Resets the display so that only the scaffold appears 
+ */
+	public void reset() {
+		hang = new Hangman();
+
+		/* initialize the ivars (80 was determined by trial and error, might be a better way to define ropeStartingY) */
+		ropeStartingY = 80;
+		canvasXCenter = getWidth() / 2;
+		bodyMidpointY = ropeStartingY + (HEAD_RADIUS * 2) + BODY_LENGTH;
+		nextBodyPart = 0;
+		
+		removeAll();
+		buildScaffold();
+		generateLabels();
+	}
+
+/*
+ * Updates the word on the screen to correspond to the current state of the game.  The argument string shows what letters have
+ * been guessed so far; unguessed letters are indicated by hyphens.
+ * 
+ * @param word	The player's word
+ * @param wrongGuessList	List of incorrectly-guessed letters
+ */
+	public void displayWord(String word, String wrongGuessList) {
+		
+		if (hang.getNumLettersRemaining() == 1) {
+			/* set the word at the bottom of the page to the secret word */
+			playerLabel.setLabel(word); 
+			playerLabel.setFont("Helvetica-20");
+			
+			/* tell the player they've won */
+			youWin.setLabel("Congratulations! You Win!");
+			youWin.setFont("Helvetica-20");
+			youWin.setColor(Color.GREEN);
+			youWin.setLocation( (canvasXCenter - (youWin.getWidth() / 2)), getHeight() /2 );
+		} else {
+			playerLabel.setLabel(word);
+			playerLabel.setFont("Helvetica-20");
+		}
+		
+		incorrectGuesses.setLabel(wrongGuessList);
+		incorrectGuesses.setFont("Helvetica-15");
+	}
+
+/*
+ * Updates the display to correspond to an incorrect guess by the user.  Calling this method causes the next body part to appear
+ * on the scaffold and adds the letter to the list of incorrect guesses that appears at the bottom of the window.
+ * 
+ * @param letter	Incorrectly-guessed letter.
+ */
+	public void noteIncorrectGuess(char letter) {
+		addNextBodyPart(nextBodyPart);
+		wrongLetters += letter + "";
+		
+		/* When the last body part is drawn, indicate player has lost */
+		if (nextBodyPart == 8) {
+			youLose.setLabel("Sorry! You Lose! Please Play Again!");
+			youLose.setFont("Helvetica-20");
+			youLose.setColor(Color.RED);
+			youLose.setLocation( (canvasXCenter - (youLose.getWidth() / 2)), getHeight() /2 );
+			
+			secretWordDisplay.setLabel("The word was: " + hang.getWordToGuess());
+			secretWordDisplay.setFont("Helvetica-20");
+			secretWordDisplay.setColor(Color.RED);
+			secretWordDisplay.setLocation( (canvasXCenter - (secretWordDisplay.getWidth() / 2)), getHeight() /1.75 );
+		}
+	}
+	
+/*
+ * Builds scaffold consisting of 3 parts: Pole, beam & rope. Scaffold never changes during the course of the game.
+ */
+	private void buildScaffold() {
+		int ropeStartingX = canvasXCenter;
+		int ropeFinalX = canvasXCenter;
+		int ropeFinalY = ropeStartingY - ROPE_LENGTH;
+		int beamFinalX = ropeFinalX - BEAM_LENGTH;
+		int poleFinalY = ropeFinalY + SCAFFOLD_HEIGHT;
+		
+		/** build scaffold based on the rope's coordinates, which are at center on x-axis */
+		GLine rope = new GLine(ropeStartingX, ropeStartingY, ropeFinalX, ropeFinalY);
+		add(rope);
+		
+		GLine beam = new GLine(ropeFinalX, ropeFinalY, beamFinalX, ropeFinalY);
+		add(beam);
+		
+		GLine pole = new GLine(beamFinalX, ropeFinalY, beamFinalX, poleFinalY );
+		add(pole);
+	}
+	
+	
+/*
+ * Adds the next body part to the scaffold when player guesses incorrectly. 
+ *  
+ *  @param nextBodyPart	number representing the next body part that should be drawn if player guesses incorrectly.
+ */
+	private void addNextBodyPart(int nextPart) {
+		switch (nextPart) {
+			case 0: 
+				drawHead(); 
+				nextBodyPart = 1;
+				break;
+			case 1:  //body
+				drawNonHeadBodyParts(canvasXCenter, bodyMidpointY, canvasXCenter, (bodyMidpointY - BODY_LENGTH) ); //body
+				drawNonHeadBodyParts(canvasXCenter - (HIP_WIDTH / 2), bodyMidpointY, canvasXCenter + (HIP_WIDTH / 2), bodyMidpointY); //bodyMidpoint
+				nextBodyPart = 2;
+				break;
+			case 2: //left arm
+				drawNonHeadBodyParts(canvasXCenter, (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD), (canvasXCenter - UPPER_ARM_LENGTH), (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD) ); //upper left arm
+				drawNonHeadBodyParts((canvasXCenter - UPPER_ARM_LENGTH), (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD), (canvasXCenter - UPPER_ARM_LENGTH), ((bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD) + LOWER_ARM_LENGTH) ); //lower left arm
+				nextBodyPart = 3;
+				break;
+			case 3: //right arm
+				drawNonHeadBodyParts( canvasXCenter, (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD), (canvasXCenter + UPPER_ARM_LENGTH),  (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD) ); //upper right arm
+				drawNonHeadBodyParts((canvasXCenter + UPPER_ARM_LENGTH), (bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD), (canvasXCenter + UPPER_ARM_LENGTH), ((bodyMidpointY - BODY_LENGTH + ARM_OFFSET_FROM_HEAD) + LOWER_ARM_LENGTH) ); //lower right arm
+				nextBodyPart = 4;
+				break;
+			case 4: //left leg
+				drawNonHeadBodyParts(canvasXCenter - (HIP_WIDTH / 2), bodyMidpointY, canvasXCenter - (HIP_WIDTH / 2), bodyMidpointY + LEG_LENGTH); 
+				nextBodyPart = 5;
+				break;
+			case 5: //right leg
+				drawNonHeadBodyParts(canvasXCenter + (HIP_WIDTH / 2), bodyMidpointY, canvasXCenter + (HIP_WIDTH / 2), bodyMidpointY + LEG_LENGTH); //right leg
+				nextBodyPart = 6;
+				break;
+			case 6: //left foot
+				drawNonHeadBodyParts(canvasXCenter - (HIP_WIDTH / 2), bodyMidpointY + LEG_LENGTH, canvasXCenter - (HIP_WIDTH / 2) - FOOT_LENGTH, bodyMidpointY + LEG_LENGTH);
+				nextBodyPart = 7;
+				break;
+			case 7: //right foot
+				drawNonHeadBodyParts(canvasXCenter + (HIP_WIDTH / 2), bodyMidpointY + LEG_LENGTH, canvasXCenter + (HIP_WIDTH / 2) + FOOT_LENGTH, bodyMidpointY + LEG_LENGTH); //right foot
+				nextBodyPart = 8;
+				break;
+			case 8: //default
+			default: throw new ErrorException("no more body parts");
+		}
+	}
+	
+	
+/*
+ * Draws the head, which is a GOval.
+ */
+	private void drawHead() {
+		GOval head = new GOval( (canvasXCenter - HEAD_RADIUS), ropeStartingY, HEAD_RADIUS * 2, HEAD_RADIUS * 2 );
+		add(head);
+	}
+	
+
+/*
+ * Draws the non-head body parts, which are all GLines
+ * 
+ * @param x1	integer for x-coord of first point in line
+ * @param y1	integer for y-coord of first point in line
+ * @param x2	integer for x-coord of second point in line
+ * @param y2	integer for y-coord of second point in line
+ */
+	private void drawNonHeadBodyParts(int x1, int y1, int x2, int y2) {
+		GLine bodyPart = new GLine(x1, y1, x2, y2);
+		add(bodyPart);
+	}
+	
+/*
+ * Generates the player's word and incorrect guesses labels
+ */
+	private void generateLabels() {
+		playerLabel = new GLabel( "", getWidth() / 8, (getHeight() - (getHeight() / 12)) );
+		add(playerLabel);
+		
+		incorrectGuesses = new GLabel( "", getWidth() / 8, (getHeight() - (getHeight() / 20)) );
+		add(incorrectGuesses);
+		
+		youLose = new GLabel( "", getWidth() / 8, (getHeight() - (getHeight() / 20)) );
+		add(youLose);
+		
+		youWin = new GLabel( "", getWidth() / 8, (getHeight() - (getHeight() / 20)) );
+		add(youWin);
+		
+		secretWordDisplay = new GLabel( "", getWidth() / 8, (getHeight() - (getHeight() / 20)) );
+		add(secretWordDisplay);
+	}
+
+/*
+ * ToString method, requirement from Lecture 9 for all non-program classes we write to have a toString() method
+ */
+	public String toString() {
+		return "This class draws the hangman if the secret word is not guessed. The secret word this time is " + secretWordDisplay;
+	}
+	
+
+/* Instance Variables */
+	private int nextBodyPart;
+	private int canvasXCenter;
+	private int bodyMidpointY;
+	private int ropeStartingY;
+	private String wrongLetters;
+	private GLabel playerLabel;
+	private GLabel incorrectGuesses;
+	private GLabel youLose;
+	private GLabel youWin;
+	private Hangman hang;
+	private GLabel secretWordDisplay;
+
+	
+}
